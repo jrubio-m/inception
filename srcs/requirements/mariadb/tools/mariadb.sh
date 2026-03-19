9d5555824442c213e98a45c65ba9ade63c23d776
jrubio-m <jrubio-m@github.com>
2026-03-19 10:34:10 +0000

    19-03-2026: Structure completed and mariadb's DockerFile, conf file and script initialized (not finished)

---
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# leer secrets
+# preparar directorios/permisos
+# comprobar si la base ya existe
+# si no:
+# 	inicializar mariadb
+# 	arrancar temporalmente
+# 	crear db y usuario
+# 	apagar temporalmente
+# arrancar mariadb en foreground
